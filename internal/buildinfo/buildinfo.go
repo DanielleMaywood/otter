8cbceef3c4e966c11204f490941bcf1043eecabb
Danielle Maywood <danielle@themaywoods.com>
2025-04-18 22:21:21 +0100

    fix: add a fallback version when other build methods are used

--- internal/buildinfo/buildinfo.go
+++ internal/buildinfo/buildinfo.go
@@ -1,12 +1,44 @@
 package buildinfo
 
-import ()
+import (
+	"runtime/debug"
+	"sync"
+)
 
 var (
 	// This value is injected at build time in ./scripts/build.sh.
 	version string
+
+	getVersion = sync.OnceValue(func() string {
+		// If version is populated we'll happily return that
+		if version != "" {
+			return version
+		}
+
+		// As version hasn't been injected at build time, we'll
+		// have to query `ReadBuildInfo` to attempt to get some
+		// information about the binary.
+		buildInfo, valid := debug.ReadBuildInfo()
+		if !valid {
+			// If we're unable to read the build info, we'll
+			// just return that we do not know the version.
+			return "unknown"
+		}
+
+		// We'll attempt to find the "vcs.revision" setting
+		// as this is still useful information.
+		for _, setting := range buildInfo.Settings {
+			if setting.Key == "vcs.revision" {
+				return setting.Value
+			}
+		}
+
+		// We haven't found anything useful so we'll again just
+		// return that we do not know the version.
+		return "unknown"
+	})
 )
 
 func Version() string {
-	return version
+	return getVersion()
 }
